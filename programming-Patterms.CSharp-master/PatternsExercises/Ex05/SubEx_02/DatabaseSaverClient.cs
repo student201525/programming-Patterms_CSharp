12e3302701d6c20ba31185cb8a775a308e070e89
student201525 <okynev.nikita@yandex.ru>
2018-06-09 01:21:27 +0500

    Update DatabaseSaverClient.cs

--- programming-Patterms.CSharp-master/PatternsExercises/Ex05/SubEx_02/DatabaseSaverClient.cs
+++ programming-Patterms.CSharp-master/PatternsExercises/Ex05/SubEx_02/DatabaseSaverClient.cs
@@ -6,13 +6,44 @@ namespace Patterns.Ex05.SubEx_02
     {
         public void Main(bool b)
         {
-            var databaseSaver = new DatabaseSaver();
+            var databaseSaver = new DatabaseSaverProxy(new DatabaseSaver());
+            databaseSaver.DataSaved += (sender, args) =>
+            {
+                var mailSender = new MailSender(); 
+                var cacheUpdated = new CacheUpdater();
+                mailSender.Send("email");
+                cacheUpdated.UpdateCache();               
+            };
             DoSmth(databaseSaver);
         }
 
         private void DoSmth(IDatabaseSaver saver)
         {
             saver.SaveData(null);
+        }
+    }
+
+    public class DatabaseSaverProxy : IDatabaseSaver
+    {
+
+        public event EventHandler DataSaved;
+
+        protected virtual void OnDataSaved()
+        {
+            DataSaved?.Invoke(this, EventArgs.Empty);
+        }
+
+        private readonly IDatabaseSaver _databaseSaver;
+
+        public DatabaseSaverProxy(IDatabaseSaver databaseSaver)
+        {
+            _databaseSaver = databaseSaver;
+        }
+
+        public void SaveData(object data)
+        {
+            _databaseSaver.SaveData(data);
+            OnDataSaved();
         }
     }
 }
