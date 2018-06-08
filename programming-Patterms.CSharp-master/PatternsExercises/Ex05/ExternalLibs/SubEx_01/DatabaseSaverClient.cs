c1131c9bc6268f0810377e69c67460d05be3dd86
student201525 <okynev.nikita@yandex.ru>
2018-06-09 01:21:54 +0500

    Update DatabaseSaverClient.cs

--- programming-Patterms.CSharp-master/PatternsExercises/Ex05/ExternalLibs/SubEx_01/DatabaseSaverClient.cs
+++ programming-Patterms.CSharp-master/PatternsExercises/Ex05/ExternalLibs/SubEx_01/DatabaseSaverClient.cs
@@ -6,13 +6,63 @@ namespace Patterns.Ex05.SubEx_01
     {
         public void Main(bool b)
         {
-            var databaseSaver = new DatabaseSaver();
+            IDatabaseSaver databaseSaver = new DatabaseSaver();
+            var sender = new MailSender();
+            databaseSaver = new MailSenderDecorator(databaseSaver, sender);
+            databaseSaver = new MailSenderDecorator(databaseSaver, sender, "nikita@yandex.ru");
+            databaseSaver = new CacheUpdaterDecorator(databaseSaver, new CacheUpdater());
             DoSmth(databaseSaver);
         }
 
         private void DoSmth(IDatabaseSaver saver)
         {
             saver.SaveData(null);
+        }
+    }
+
+    public class DatabaseSaverDecorator : IDatabaseSaver
+    {
+        protected IDatabaseSaver Wrappee;
+
+        public DatabaseSaverDecorator(IDatabaseSaver saver)
+        {
+            Wrappee = saver;
+        }
+
+        public void SaveData(object data)
+        {
+           Wrappee.SaveData(data);
+        }
+    }
+    public class MailSenderDecorator : DatabaseSaverDecorator
+    {
+        private readonly MailSender _mailSender;
+        private readonly string _email;
+        public MailSenderDecorator(IDatabaseSaver saver, MailSender mailSender, string email = "") : base(saver)
+        {
+            _mailSender = mailSender;
+            _email = email;
+        }
+
+        public new void SaveData(object data)
+        {
+            base.SaveData(data);
+            _mailSender.Send(_email);
+        }
+    }
+
+    public class CacheUpdaterDecorator : DatabaseSaverDecorator
+    {
+        private readonly CacheUpdater _cacheUpdater;
+        public CacheUpdaterDecorator(IDatabaseSaver saver, CacheUpdater cacheUpdater) : base(saver)
+        {
+            _cacheUpdater = cacheUpdater;
+        }
+
+        public new void SaveData(object data)
+        {
+            base.SaveData(data);
+            _cacheUpdater.UpdateCache();
         }
     }
 }
